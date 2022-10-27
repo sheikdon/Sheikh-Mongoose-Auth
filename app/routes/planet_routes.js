9fd561a1923b24f02e23a9b85a4d9c3340aa1620
sheikdon <sheikdonabdiresac@gmail.com>
2022-10-27 07:48:07 -0500

    day 2: added routes and apiUrl

--- app/routes/planet_routes.js
+++ app/routes/planet_routes.js
@@ -4,7 +4,7 @@ const express = require('express')
 const passport = require('passport')
 
 // pull in Mongoose model for examples
-const Pet = require('../models/planet')
+const Planet = require('../models/planet')
 
 // this is a collection of methods that help us detect situations when we need
 // to throw a custom error
@@ -19,7 +19,7 @@ const requireOwnership = customErrors.requireOwnership
 // this is middleware that will remove blank fields from `req.body`, e.g.
 // { example: { title: '', text: 'foo' } } -> { example: { text: 'foo' } }
 const removeBlanks = require('../../lib/remove_blank_fields')
-const pet = require('../models/planet')
+const planet = require('../models/planet')
 const planet = require('../models/planet')
 // passing this as a second argument to `router.<verb>` will make it
 // so that a token MUST be passed for that route to be available
@@ -29,19 +29,75 @@ const requireToken = passport.authenticate('bearer', { session: false })
 // instantiate a router (mini app that only handles routes)
 const router = express.Router()
 
-//create
-//pet
-router.post('/planet', requireToken, (req,res, next) => {
-    req.body.planet.owner = req.user.id
-
-    //
-    Planet.create(req.body.planet)
-    .then(planet => {
-        res.status(201).json({ planet: planet})
+//Index
+// /planets
+router.get('/planets', requireToken, (req, res, next) => {
+    Planet.find()
+    .then(planets => {
+        return planets.map(planet => planet)
+    })
+    .then(planets =>  {
+        res.status(200).json({ planets: planets })
     })
     .catch(next)
-        
-}) 
+})
+//show
+// /planets/:id
+router.get('/planets/:id', requireToken, (req, res, next) => {
+    Planet.findById(req.params.id)
+    .then(handle404)
+    .then(planet => {
+        res.status(200).json({ planet: planet})
+    })
+    .catch(next)
+})
+
+//create
+//planet
+router.post('/planets', requireToken, (req, res, next) => {
+    req.body.planet.owner = req.user.id
+
+    // one the front end I HAVE TO SEND a planet as the top level key
+    // planet: {name: '', type: ''}
+    Planet.create(req.body.planet)
+    .then(planet => {
+        res.status(201).json({ planet: planet })
+    })
+    .catch(next)
+    // .catch(error => next(error))
+
+})
+//update
+// /planets/:id
+router.patch('/planets/:id', requireToken, (req, res, next) =>{
+    delete req.body.planet.owner
+
+    Planet.findById(req.params.id)
+    .then(handle404)
+    .then(planet => {
+        requireOwnership(req, planet)
+
+        return planet.updateOne(req.body.planet)
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+//Delete
+// /planets/:id
+router.delete('/planets/:id', requireToken, (req, res, next) => {
+	Planet.findById(req.params.id)
+		.then(handle404)
+		.then((planet) => {
+			// throw an error if current user doesn't own `example`
+			requireOwnership(req, planet)
+			// delete the example ONLY IF the above didn't throw
+			planet.deleteOne()
+		})
+		// send back 204 and no content if the deletion succeeded
+		.then(() => res.sendStatus(204))
+		// if an error occurs, pass it to the handler
+		.catch(next)
+})
 
 
 
